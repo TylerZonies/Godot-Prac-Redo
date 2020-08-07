c0dcd181318e8d7875f9046a550f1e1d66f04b00
Tyler Zonies <tylerzonies@gmail.com>
2020-08-06 22:41:31 -0600

    added some enemy animations

--- game/scenes/projectile.gd
+++ game/scenes/projectile.gd
@@ -1,14 +1,14 @@
 extends RigidBody2D
 
 const PROJECTILE_SPEED = 800
-const LIFETIME = 0.8
+export var lifeTime = 0.3
 
 func _ready():
 	apply_impulse(Vector2(), Vector2(PROJECTILE_SPEED, 0).rotated(rotation))
 	destroy()
 	
 func destroy():
-	yield(get_tree().create_timer(LIFETIME), "timeout")
+	yield(get_tree().create_timer(lifeTime), "timeout")
 	queue_free()
 
 func _on_projectile_body_entered(body):
