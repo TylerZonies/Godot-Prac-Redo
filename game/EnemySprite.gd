33659e1ec19fde368a147009b453d3d23ff42c4d
Tyler Zonies <tylerzonies@gmail.com>
2020-08-06 22:41:54 -0600

    actually added the animations

--- game/EnemySprite.gd
+++ game/EnemySprite.gd
@@ -1,16 +1,16 @@
 extends AnimatedSprite
 
+var direction = 'Right'
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func move(motion):
+	if motion.x > 0:
+		if motion.y > 0:
+			direction = 'RightDown'
+		elif motion.y < 0:
+			direction = 'RightUp'
+		else:
+			direction = 'Right'
+	
+	if motion.x != 0 && motion.y != 0:
+		play('walk' + direction)
+	if
